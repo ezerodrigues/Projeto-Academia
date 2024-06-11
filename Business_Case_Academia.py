21d3787be71ce78aaa86d8155be3a378700ebedb
ezerodrigues <ezerodrigues@msn.com>
2024-06-11 09:40:44 -0300

    Projeto Modificado

--- Business_Case_Academia.py
+++ Business_Case_Academia.py
@@ -1,10 +1,5 @@
 
-from fpdf import FPDF
-
-
-def imc (peso_i, altura_i):
-    return peso_i / (altura_i ** 2)
-
+import pdf_reports
 
 
 nome = input ('Digite seu nome: ')
@@ -13,27 +8,22 @@ peso = float(input('Digite o seu peso: '))
 altura = float(input ('Digite a sua altura: '))
 
 
-calculo = imc(peso, altura)
+imc = float(peso) / (float(altura) ** 2)
 
+status = 'dentro do peso'
 
-# Criação do PDF
+if imc > 25:
+    status = "sobrepeso"
 
-pdf = FPDF()
-pdf.add_page()
-pdf.set_font("Arial", size=12)
+dados_aluno = {
+    "nome" : nome,
+    "idade" : idade,
+    "peso" : peso,
+    "altura": altura,
+    "imc" : round(imc, 2),
+    "status" : status
+}
 
+template = pdf_reports.pug_to_html("template.pug", dados = dados_aluno)
 
-# Adicionando texto ao PDF
-
-pdf.text(10, 10, f'Nome: {nome}')
-pdf.text(10, 20, f'Idade: {idade}')
-pdf.text(10, 30, f'Peso: {peso} kg')
-pdf.text(10, 40, f'Altura: {altura} m')
-pdf.text(10, 50, f'IMC: {calculo:.2f}')
-
-
-
-# Salvando o PDF
-
-pdf.output("Ficha Aluno.pdf")
-print("Dados gerados com sucesso!")
+pdf_reports.write_report(template, "ficha_aluno.pdf", use_default_styling=False)
